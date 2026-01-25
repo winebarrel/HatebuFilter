7f9176033da13936984434484117c96359a48314
winebarrel <sugawara@winebarrel.jp>
2026-01-25 18:06:17 +0900

    first implementation

--- HatebuFilter Extension/Resources/background.js
+++ HatebuFilter Extension/Resources/background.js
@@ -1,6 +1,13 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-
-    if (request.greeting === "hello")
-        return Promise.resolve({ farewell: "goodbye" });
-});
+browser.runtime.onMessage.addListener(
+  async (request, _sender, sendResponse) => {
+    switch (request.message) {
+      case "load":
+        const { filter } = await chrome.storage.local.get("filter");
+        await sendResponse({ filter: filter ?? "" });
+        break;
+      case "store":
+        await chrome.storage.local.set({ filter: request.filter ?? "" });
+        break;
+    }
+  }
+);
