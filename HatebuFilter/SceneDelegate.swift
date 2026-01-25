5a60db4db012852d7df0ac1299ed092666a9fc3e
winebarrel <sugawara@winebarrel.jp>
2026-01-25 17:40:03 +0900

    Add Xcode project

---
+++ HatebuFilter/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  HatebuFilter
+//
+//  Created by 菅原元気 on 2026/01/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
